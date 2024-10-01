04339171cc0c77c848141089a3d2a115e888ac3c
Javier Paz <javierbrassai@gmail.com>
2024-10-01 13:28:49 +0200

    Validaciones y migracion de datos

--- app/Http/Controllers/Auth/AccountController.php
+++ app/Http/Controllers/Auth/AccountController.php
@@ -10,7 +10,13 @@ class AccountController extends Controller
     public function index(){
         return view('account');
     }
-    public function store(){
-        dd('POST');
+    public function store(Request $request){
+        $this->validate($request,[
+            'name'=>'required',
+            'userName'=>'required|unique:users',
+            'email'=>'required|unique:users|email',
+            'password'=>'required|min:4|confirmed'
+        ]);
+        dd('Cuenta creada');
     }
 }
--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -14,18 +14,30 @@
                 <div>
                     <label for="name">Nombre</label>
                     <input id="name" name="name" type="text" placeholder="Tu nombre" class="border">
+                    @error('name')
+                    <p>{{$message}}</p>
+                    @enderror
                 </div>
                 <div>
                     <label for="userName">Nombre de usuario</label>
-                    <input id="userName" name="userName" type="text" placeholder="Tu nombre de usuario" class="border">
+                    <input id="userName" name="userName" type="text" placeholder="Tu nombre de usuario" class="border" value="{{old('userName')}}">
+                    @error('userName')
+                    <p>{{$message}}</p>
+                    @enderror
                 </div>
                 <div>
                     <label for="email">Email</label>
                     <input id="email" name="email" type="email" placeholder="Tu Email" class="border">
+                    @error('email')
+                    <p>{{$message}}</p>
+                    @enderror
                 </div>
                 <div>
                     <label for="password">Password</label>
                     <input id="password" name="password" type="password" placeholder="Tu password" class="border">
+                    @error('password')
+                    <p>{{$message}}</p>
+                    @enderror
                 </div>
                 <div>
                     <label for="password_confirmation">Repetir Password</label>
@@ -36,3 +48,10 @@
         </div>
     </div>
 @endsection
+<script>
+    document.addEventListener('input', function() {
+        if (this.value === 'null') {
+            this.style.borderColor = 'red';
+        }
+    });
+</script>
